613dfaa7d5957c62484dc772116b1433c61de739
nguyenletan <nguyenletan@gmail.com>
2022-12-07 01:20:44 +1100

    added to vercel

--- dev.js
+++ dev.js
@@ -2,4 +2,4 @@ import app from "./api/index.js";
 import consola from "consola";
 import dotenv from "dotenv";
 dotenv.config();
-app.listen(3000, () => consola.info("Server started"));
+app.listen(4000, () => consola.info("Server started"));

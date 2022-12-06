6d77d71a7f9258a28157558b26b86f1aef452761
nguyenletan <nguyenletan@gmail.com>
2022-12-07 01:17:40 +1100

    added to vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
@@ -7,36 +6,7 @@ import http from 'http';
 import cors from 'cors';
 import pkg from 'body-parser';
 const { json } = pkg;
-//import { typeDefs, resolvers } from './schema';
-
-
-
-const apartmentsFromGuesty = [
-  {
-    id: '631815d66781f70033d49b10',
-    title: "Griffith Wood - One Bedroom Apartment",
-  }
-]
-
-const typeDefs = `#graphql
-  type Apartment {
-    id: String
-    title: String
-  }
-  
-  type Query {
-    hello: String
-    apartments: [Apartment]
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => 'world',
-    apartments: () => apartmentsFromGuesty,
-  },
-}
-
+import { typeDefs, resolvers } from './schema.js';
 const app = express();
 const httpServer = http.createServer(app);
 const server = new ApolloServer({
